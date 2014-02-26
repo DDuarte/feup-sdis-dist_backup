b3976c6a0d905e8e3941befee70924780f4d0f7f
Duarte Duarte <dnpd.dd@gmail.com>
2014-02-26 20:51:25 +0000

    Create NetworkUtilities class with method GetLocalIPAddresses()

---
+++ DBS/NetworkUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using DBS.Annotations;
+
+namespace DBS
+{
+    [UsedImplicitly]
+    public class NetworkUtilities
+    {
+        /// <summary>
+        /// Returns all IPv4 local IP addresses
+        /// </summary>
+        /// <returns>Array of addresses</returns>
+        [UsedImplicitly]
+        public static IPAddress[] GetLocalIPAddresses()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            return host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToArray();
+        }
+    }
+}
