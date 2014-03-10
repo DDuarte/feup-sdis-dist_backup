f9f86fc6f8d14589f2b035ce8c33159cb1f3c4a6
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-03-10 11:22:27 +0000

    Add more unit tests for the boundaries of the field values:
    - Upper bounds;
    - Lower bounds;
    - Out of range values.

--- DBSTests/MessageTests.cs
+++ DBSTests/MessageTests.cs
@@ -91,5 +91,162 @@ namespace DBSTests
             Assert.AreEqual(msg.ReplicationDeg, msg2.ReplicationDeg);
             CollectionAssert.AreEqual(msg.Body, msg2.Body);
         }
+
+        [TestMethod]
+        public void TestMessageFieldUpperBounds()
+        {
+            var body = new byte[255];
+            for (var i = 0; i < body.Length; ++i)
+                    body[i] = (byte)i;
+
+
+            var getChunkMsg = Message.BuildGetChunkMessage(9, 9, FileId1, 999999);
+            var bytes = getChunkMsg.Serialize();
+            var getChunkMsg2 = Message.Deserialize(bytes);
+            Assert.AreEqual(getChunkMsg.MessageType, getChunkMsg2.MessageType);
+            Assert.AreEqual(getChunkMsg.VersionM, getChunkMsg2.VersionM);
+            Assert.AreEqual(getChunkMsg.VersionN, getChunkMsg2.VersionN);
+            CollectionAssert.AreEqual(getChunkMsg.FileId, getChunkMsg2.FileId);
+            Assert.AreEqual(getChunkMsg.ChunkNo, getChunkMsg2.ChunkNo);
+            Assert.AreEqual(getChunkMsg.ReplicationDeg, getChunkMsg2.ReplicationDeg);
+            CollectionAssert.AreEqual(getChunkMsg.Body, getChunkMsg2.Body);
+
+            var chunkMsg = Message.BuildChunkMessage(9, 9, FileId1, 999999, body);
+            bytes = chunkMsg.Serialize();
+            var chunkMsg2 = Message.Deserialize(bytes);
+            Assert.AreEqual(chunkMsg.MessageType, chunkMsg2.MessageType);
+            Assert.AreEqual(chunkMsg.VersionM, chunkMsg2.VersionM);
+            Assert.AreEqual(chunkMsg.VersionN, chunkMsg2.VersionN);
+            CollectionAssert.AreEqual(chunkMsg.FileId, chunkMsg2.FileId);
+            Assert.AreEqual(chunkMsg.ChunkNo, chunkMsg2.ChunkNo);
+            Assert.AreEqual(chunkMsg.ReplicationDeg, chunkMsg2.ReplicationDeg);
+            CollectionAssert.AreEqual(chunkMsg.Body, chunkMsg2.Body);
+
+            var putChunkMsg = Message.BuildPutChunkMessage(9, 9, FileId1, 999999, 9, body);
+            bytes = putChunkMsg.Serialize();
+            var putChunkMsg2 = Message.Deserialize(bytes);
+            Assert.AreEqual(putChunkMsg.MessageType, putChunkMsg2.MessageType);
+            Assert.AreEqual(putChunkMsg.VersionM, putChunkMsg2.VersionM);
+            Assert.AreEqual(putChunkMsg.VersionN, putChunkMsg2.VersionN);
+            CollectionAssert.AreEqual(putChunkMsg.FileId, putChunkMsg2.FileId);
+            Assert.AreEqual(putChunkMsg.ChunkNo, putChunkMsg2.ChunkNo);
+            Assert.AreEqual(putChunkMsg.ReplicationDeg, putChunkMsg2.ReplicationDeg);
+            CollectionAssert.AreEqual(putChunkMsg.Body, putChunkMsg2.Body);
+
+            var storedMsg = Message.BuildStoredMessage(9, 9, FileId1, 999999);
+            bytes = storedMsg.Serialize();
+            var storedMsg2 = Message.Deserialize(bytes);
+            Assert.AreEqual(storedMsg.MessageType, storedMsg2.MessageType);
+            Assert.AreEqual(storedMsg.VersionM, storedMsg2.VersionM);
+            Assert.AreEqual(storedMsg.VersionN, storedMsg2.VersionN);
+            CollectionAssert.AreEqual(storedMsg.FileId, storedMsg2.FileId);
+            Assert.AreEqual(storedMsg.ChunkNo, storedMsg2.ChunkNo);
+            Assert.AreEqual(storedMsg.ReplicationDeg, storedMsg2.ReplicationDeg);
+            CollectionAssert.AreEqual(storedMsg.Body, storedMsg2.Body);
+
+            var deleteMsg = Message.BuildDeleteMessage(FileId1);
+            bytes = deleteMsg.Serialize();
+            var deleteMsg2 = Message.Deserialize(bytes);
+            Assert.AreEqual(deleteMsg.MessageType, deleteMsg2.MessageType);
+            Assert.AreEqual(deleteMsg.VersionM, deleteMsg2.VersionM);
+            Assert.AreEqual(deleteMsg.VersionN, deleteMsg2.VersionN);
+            CollectionAssert.AreEqual(deleteMsg.FileId, deleteMsg2.FileId);
+            Assert.AreEqual(deleteMsg.ChunkNo, deleteMsg2.ChunkNo);
+            Assert.AreEqual(deleteMsg.ReplicationDeg, deleteMsg2.ReplicationDeg);
+            CollectionAssert.AreEqual(deleteMsg.Body, deleteMsg2.Body);
+
+            var removedMsg = Message.BuildRemovedMessage(9, 9, FileId1, 999999);
+            bytes = removedMsg.Serialize();
+            var removedMsg2 = Message.Deserialize(bytes);
+            Assert.AreEqual(removedMsg.MessageType, removedMsg2.MessageType);
+            Assert.AreEqual(removedMsg.VersionM, removedMsg2.VersionM);
+            Assert.AreEqual(removedMsg.VersionN, removedMsg2.VersionN);
+            CollectionAssert.AreEqual(removedMsg.FileId, removedMsg2.FileId);
+            Assert.AreEqual(removedMsg.ChunkNo, removedMsg2.ChunkNo);
+            Assert.AreEqual(removedMsg.ReplicationDeg, removedMsg2.ReplicationDeg);
+            CollectionAssert.AreEqual(removedMsg.Body, removedMsg2.Body);
+        }
+
+        [TestMethod]
+        public void TestMessageFieldLowerBounds()
+        {
+            var body = new byte[255];
+            for (var i = 0; i < body.Length; ++i)
+                body[i] = (byte)i;
+
+
+            var getChunkMsg = Message.BuildGetChunkMessage(0, 0, FileId1, 0);
+            var bytes = getChunkMsg.Serialize();
+            var getChunkMsg2 = Message.Deserialize(bytes);
+            Assert.AreEqual(getChunkMsg.MessageType, getChunkMsg2.MessageType);
+            Assert.AreEqual(getChunkMsg.VersionM, getChunkMsg2.VersionM);
+            Assert.AreEqual(getChunkMsg.VersionN, getChunkMsg2.VersionN);
+            CollectionAssert.AreEqual(getChunkMsg.FileId, getChunkMsg2.FileId);
+            Assert.AreEqual(getChunkMsg.ChunkNo, getChunkMsg2.ChunkNo);
+            Assert.AreEqual(getChunkMsg.ReplicationDeg, getChunkMsg2.ReplicationDeg);
+            CollectionAssert.AreEqual(getChunkMsg.Body, getChunkMsg2.Body);
+
+            var chunkMsg = Message.BuildChunkMessage(0, 0, FileId1, 0, body);
+            bytes = chunkMsg.Serialize();
+            var chunkMsg2 = Message.Deserialize(bytes);
+            Assert.AreEqual(chunkMsg.MessageType, chunkMsg2.MessageType);
+            Assert.AreEqual(chunkMsg.VersionM, chunkMsg2.VersionM);
+            Assert.AreEqual(chunkMsg.VersionN, chunkMsg2.VersionN);
+            CollectionAssert.AreEqual(chunkMsg.FileId, chunkMsg2.FileId);
+            Assert.AreEqual(chunkMsg.ChunkNo, chunkMsg2.ChunkNo);
+            Assert.AreEqual(chunkMsg.ReplicationDeg, chunkMsg2.ReplicationDeg);
+            CollectionAssert.AreEqual(chunkMsg.Body, chunkMsg2.Body);
+
+            var putChunkMsg = Message.BuildPutChunkMessage(0, 0, FileId1, 0, 0, body);
+            bytes = putChunkMsg.Serialize();
+            var putChunkMsg2 = Message.Deserialize(bytes);
+            Assert.AreEqual(putChunkMsg.MessageType, putChunkMsg2.MessageType);
+            Assert.AreEqual(putChunkMsg.VersionM, putChunkMsg2.VersionM);
+            Assert.AreEqual(putChunkMsg.VersionN, putChunkMsg2.VersionN);
+            CollectionAssert.AreEqual(putChunkMsg.FileId, putChunkMsg2.FileId);
+            Assert.AreEqual(putChunkMsg.ChunkNo, putChunkMsg2.ChunkNo);
+            Assert.AreEqual(putChunkMsg.ReplicationDeg, putChunkMsg2.ReplicationDeg);
+            CollectionAssert.AreEqual(putChunkMsg.Body, putChunkMsg2.Body);
+
+            var storedMsg = Message.BuildStoredMessage(0, 0, FileId1, 0);
+            bytes = storedMsg.Serialize();
+            var storedMsg2 = Message.Deserialize(bytes);
+            Assert.AreEqual(storedMsg.MessageType, storedMsg2.MessageType);
+            Assert.AreEqual(storedMsg.VersionM, storedMsg2.VersionM);
+            Assert.AreEqual(storedMsg.VersionN, storedMsg2.VersionN);
+            CollectionAssert.AreEqual(storedMsg.FileId, storedMsg2.FileId);
+            Assert.AreEqual(storedMsg.ChunkNo, storedMsg2.ChunkNo);
+            Assert.AreEqual(storedMsg.ReplicationDeg, storedMsg2.ReplicationDeg);
+            CollectionAssert.AreEqual(storedMsg.Body, storedMsg2.Body);
+
+            var deleteMsg = Message.BuildDeleteMessage(FileId1);
+            bytes = deleteMsg.Serialize();
+            var deleteMsg2 = Message.Deserialize(bytes);
+            Assert.AreEqual(deleteMsg.MessageType, deleteMsg2.MessageType);
+            Assert.AreEqual(deleteMsg.VersionM, deleteMsg2.VersionM);
+            Assert.AreEqual(deleteMsg.VersionN, deleteMsg2.VersionN);
+            CollectionAssert.AreEqual(deleteMsg.FileId, deleteMsg2.FileId);
+            Assert.AreEqual(deleteMsg.ChunkNo, deleteMsg2.ChunkNo);
+            Assert.AreEqual(deleteMsg.ReplicationDeg, deleteMsg2.ReplicationDeg);
+            CollectionAssert.AreEqual(deleteMsg.Body, deleteMsg2.Body);
+
+            var removedMsg = Message.BuildRemovedMessage(0, 0, FileId1, 0);
+            bytes = removedMsg.Serialize();
+            var removedMsg2 = Message.Deserialize(bytes);
+            Assert.AreEqual(removedMsg.MessageType, removedMsg2.MessageType);
+            Assert.AreEqual(removedMsg.VersionM, removedMsg2.VersionM);
+            Assert.AreEqual(removedMsg.VersionN, removedMsg2.VersionN);
+            CollectionAssert.AreEqual(removedMsg.FileId, removedMsg2.FileId);
+            Assert.AreEqual(removedMsg.ChunkNo, removedMsg2.ChunkNo);
+            Assert.AreEqual(removedMsg.ReplicationDeg, removedMsg2.ReplicationDeg);
+            CollectionAssert.AreEqual(removedMsg.Body, removedMsg2.Body);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMessageOutOfRangeField()
+        {
+            var getChunkMsg = Message.BuildGetChunkMessage(10, 10, FileId1, 0);
+        }
     }
 }
