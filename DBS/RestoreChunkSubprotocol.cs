ce94a62834a18b096fdbd2189cd531181bc3c035
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2014-03-22 20:30:19 +0000

    Add RestoreChunkSubprotocol class.
    Add RestoreFileProtocol class.

---
+++ DBS/RestoreChunkSubprotocol.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBS
+{
+    /// <summary>
+    /// Launches the initiator-peer Chunk Restore Protocol
+    /// </summary>
+    class RestoreChunkSubprotocol : IProtocol
+    {
+        private readonly FileId _fileId;
+        private readonly int _chunkNo;
+        public Message Chunk { get; private set; }
+
+        public RestoreChunkSubprotocol(FileId fileId, int chunkNo)
+        {
+            _fileId = fileId;
+            _chunkNo = chunkNo;
+        }
+
+        private void SendGetChunk()
+        {
+            Core.Instance.MCChannel.Send(Message.BuildGetChunkMessage(_fileId, _chunkNo));
+
+            // wait for response
+            Chunk = Core.Instance.MDRChannel.Received.Where(message =>
+                message.MessageType == MessageType.Chunk &&
+                message.ChunkNo == _chunkNo &&
+                message.FileId == _fileId)
+                .Timeout(System.TimeSpan.FromMilliseconds(5000))
+                .Next().First();
+        }
+
+        public void Run()
+        {
+            SendGetChunk();
+        }
+    }
+}
---
+++ DBS/RestoreFileProtocol.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBS
+{
+    class RestoreFileProtocol : IProtocol
+    {
+        private readonly string _fileName;
+        private readonly FileEntry _fileInfo;
+
+        public RestoreFileProtocol(string fileName, FileEntry fileInfo)
+        {
+            _fileName = fileName;
+            _fileInfo = fileInfo;
+        }
+
+        public void Run()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                using (var file = File.OpenWrite(_fileName))
+                {
+                    int chunkNo = 0;
+                    Message chunk;
+                    do
+                    {
+                        var restoreChunkProtocol = new RestoreChunkSubprotocol(_fileInfo.FileId, chunkNo);
+                        restoreChunkProtocol.Run();
+                        chunk = restoreChunkProtocol.Chunk;
+                        file.Write(chunk.Body, 0, chunk.Body.Length);
+                        ++chunkNo;
+                    } while (chunk.Body.Length == 64000);
+                }
+            });
+        }
+    }
+}
+
