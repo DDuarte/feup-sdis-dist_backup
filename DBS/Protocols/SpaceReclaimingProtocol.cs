e40a72fa67e5d4dc7a234fb305f2e23be9effb84
Duarte Duarte <dnpd.dd@gmail.com>
2014-03-23 03:55:39 +0000

    Implement SpaceReclaimingProtocol and SpaceReclaimingService

---
+++ DBS/Protocols/SpaceReclaimingProtocol.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DBS.Protocols
+{
+    /// <summary>
+    /// Launches the space reclaiming protocol (deletes a file and sends a REMOVED)
+    /// </summary>
+    public class SpaceReclaimingProtocol : IProtocol
+    {
+        private readonly FileChunk _fileChunk;
+
+        public SpaceReclaimingProtocol(FileChunk fileChunk)
+        {
+            _fileChunk = fileChunk;
+        }
+
+        public void Run()
+        {
+            if (!_fileChunk.Delete())
+            {
+                Console.WriteLine("SpaceReclaimingProtocol: Could not delete file {0}", _fileChunk);
+                return;
+            }
+
+            var msg = Message.BuildRemovedMessage(_fileChunk);
+            Core.Instance.MCChannel.Send(msg);
+        }
+    }
+}
---
+++ DBS/Protocols/SpaceReclaimingService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DBS.Persistence;
+
+namespace DBS.Protocols
+{
+    /// <summary>
+    /// Listens to REMOVED messages on MC
+    /// </summary>
+    public class SpaceReclaimingService : IService
+    {
+        public void Start()
+        {
+            Core.Instance.MCChannel.Received
+                .Where(message => message.MessageType == MessageType.Removed)
+                .Subscribe(this);
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(Message msg)
+        {
+            if (msg.FileId == null)
+            {
+                Console.WriteLine("SpaceReclaimingService: bad msg, ChunkNo has no value.");
+                return;
+            }
+
+            if (!msg.ChunkNo.HasValue)
+            {
+                Console.WriteLine("BackupChunkService: bad msg, ChunkNo has no value.");
+                return;
+            }
+
+            var fileChunk = new FileChunk(msg.FileId, msg.ChunkNo.Value);
+
+            Core.Instance.Store.DecrementActualDegree(fileChunk.FileName);
+            ReplicationDegrees rd;
+            Core.Instance.Store.TryGetDegrees(fileChunk.FileName, out rd);
+            if (rd.ActualDegree < rd.WantedDegree)
+            {
+                try
+                {
+                    var putChunkReceived = false;
+                    var disposable = Core.Instance.MDBChannel.Received.Where(message =>
+                        message.MessageType == MessageType.PutChunk &&
+                        message.ChunkNo == fileChunk.ChunkNo &&
+                        message.FileId == fileChunk.FileId).Subscribe(_ => putChunkReceived = true);
+
+                    Thread.Sleep(Core.Instance.Rnd.Next(0, 401)); // random delay uniformly distributed between 0 and 400 ms
+
+                    if (!putChunkReceived)
+                        new BackupChunkSubprotocol(fileChunk, rd.WantedDegree, fileChunk.GetData()).Run();
+
+                    disposable.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("RestoreChunkService: " + ex);
+                }
+            }
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
