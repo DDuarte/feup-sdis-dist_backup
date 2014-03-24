8e3efa1ccdb66a75b1d4484dc3aadaa31a77e272
Duarte Duarte <dnpd.dd@gmail.com>
2014-03-24 22:57:12 +0000

    Add DeleteFileProtocol

---
+++ DBS/Protocols/DeleteFileProtocol.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DBS.Protocols
+{
+    public class DeleteFileProtocol : IProtocol
+    {
+        private readonly FileEntry _fileEntry;
+
+        public DeleteFileProtocol(FileEntry fileEntry)
+        {
+            if (fileEntry == null)
+                throw new ArgumentNullException("fileEntry");
+
+            _fileEntry = fileEntry;
+        }
+
+        public Task Run()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var msg = Message.BuildDeleteMessage(_fileEntry.FileId);
+                Core.Instance.MCChannel.Send(msg);
+            });
+        }
+    }
+}
