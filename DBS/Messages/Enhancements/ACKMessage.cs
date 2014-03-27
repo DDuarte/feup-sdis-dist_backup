fb65de4192eeed9629694d16510ddebffceafbf3
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-03-27 16:31:32 +0000

    Remove ACKMessage.ChunkOwnerIP and ACKMessage.ChunkOwnerPort properties.

--- DBS/Messages/Enhancements/ACKMessage.cs
+++ DBS/Messages/Enhancements/ACKMessage.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Net;
 using System.Text;
 using DBS.Utilities;
 
@@ -8,41 +7,34 @@ namespace DBS.Messages.Enhancements
 {
     class ACKMessage : Message // <Version> <FileId> <ChunkNo> <ChunkOwnerIP> <ChunkOwnerPort> <CRLF> <CRLF>
     {
-        public ACKMessage(FileChunk fileChunk, IPAddress chunkOwnerIp, int chunkOwnerPort)
+        public ACKMessage(FileChunk fileChunk)
             : this(Core.Instance.Config.VersionM, Core.Instance.Config.VersionN,
-            fileChunk.FileId, fileChunk.ChunkNo, chunkOwnerIp, chunkOwnerPort)
+            fileChunk.FileId, fileChunk.ChunkNo)
         { }
 
-        public ACKMessage(int versionM, int versionN, FileId fileId, int chunkNo,
-            IPAddress chunkOwnerIp, int chunkOwnerPort)
+        public ACKMessage(int versionM, int versionN, FileId fileId, int chunkNo)
             : base(MessageType.ACK)
         {
             ValidateVersionPart(versionM);
             ValidateVersionPart(versionN);
             ValidateFileId(fileId);
             ValidateChunkNo(chunkNo);
-            ValidateIP(chunkOwnerIp);
-            ValidatePort(chunkOwnerPort);
 
             VersionM = versionM;
             VersionN = versionN;
             FileId = fileId;
             ChunkNo = chunkNo;
-            ChunkOwnerIP = chunkOwnerIp;
-            ChunkOwnerPort = chunkOwnerPort;
         }
 
         public int VersionM { get; private set; }
         public int VersionN { get; private set; }
         public FileId FileId { get; private set; }
         public int ChunkNo { get; private set; }
-        public IPAddress ChunkOwnerIP { get; private set; }
-        public int ChunkOwnerPort { get; private set; }
 
         public override string ToString()
         {
-            return string.Format("{0} {1}#{2} {3}:{4}", MessageType,
-                FileId.ToStringSmall(), ChunkNo, ChunkOwnerIP, ChunkOwnerPort);
+            return string.Format("{0} {1}#{2} {3}", MessageType,
+                FileId.ToStringSmall(), ChunkNo, RemoteEndPoint.Address);
         }
 
         public override byte[] Serialize()
@@ -58,10 +50,6 @@ namespace DBS.Messages.Enhancements
                 stream.WriteASCII(FileId.ToString());
                 stream.WriteASCII(' ');
                 stream.WriteASCII(ChunkNo.ToString("D"));
-                stream.WriteASCII(' ');
-                stream.WriteASCII(ChunkOwnerIP.ToString());
-                stream.WriteASCII(' ');
-                stream.WriteASCII(ChunkOwnerPort.ToString("D"));
                 stream.WriteASCII("\r\n\r\n");
                 return stream.ToArray();
             }
@@ -75,22 +63,19 @@ namespace DBS.Messages.Enhancements
                 var header = reader.ReadLine(); // until crlf
                 if (header == null) return null;
                 var fields = header.Split(' '); // fields[0] is type
-                if (fields.Length != 6)
+                if (fields.Length != 4)
                     return null;
 
-                int versionM, versionN, chunkNo, port;
+                int versionM, versionN, chunkNo;
                 FileId fileId;
-                IPAddress ip;
 
                 if (!ParseVersion(fields[1], out versionM, out versionN)) return null;
                 if (!ParseFileId(fields[2], out fileId)) return null;
                 if (!ParseInt(fields[3], out chunkNo)) return null;
-                if (!ParseIP(fields[4], out ip)) return null;
-                if (!ParseInt(fields[5], out port)) return null;
 
                 try
                 {
-                    return new ACKMessage(versionM, versionN, fileId, chunkNo, ip, port);
+                    return new ACKMessage(versionM, versionN, fileId, chunkNo);
                 }
                 catch (Exception ex)
                 {
