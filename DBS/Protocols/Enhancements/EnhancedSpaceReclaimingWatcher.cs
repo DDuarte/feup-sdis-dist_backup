9cd8597c2965d1a713b724fc293b8d2b8fa246a2
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-03-27 18:49:44 +0000

    Add EnhancedSpaceReclaimingWatcher class.

---
+++ DBS/Protocols/Enhancements/EnhancedSpaceReclaimingWatcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBS.Messages;
+using DBS.Persistence;
+
+namespace DBS.Protocols.Enhancements
+{
+    class EnhancedSpaceReclaimingWatcher
+    {
+        private const int MinutesTimeSpan = 1;
+        public void Start()
+        {
+            Observable.Interval(TimeSpan.FromMinutes(MinutesTimeSpan)).Subscribe(_ => CheckChunks());
+        }
+
+        private static void CheckChunks()
+        {
+            foreach (var chunk in Core.Instance.Store.Where(pair => pair.Value.ActualDegree < pair.Value.WantedDegree))
+            {
+                var chunk1 = chunk;
+                try
+                {
+                    var fileChunk = new FileChunk(chunk1.Key);
+                    new BackupChunkSubprotocol(fileChunk, chunk1.Value.WantedDegree, fileChunk.GetData()).Run().Wait();
+                }
+                catch (Exception ex)
+                {
+                    Core.Instance.Log.Error("CheckChunks", ex);
+                }
+            }
+        }
+    }
+}
