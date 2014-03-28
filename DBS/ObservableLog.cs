c0450b9c03778ce918835bf710f33afd655ed4f7
Duarte Duarte <dnpd.dd@gmail.com>
2014-03-28 23:16:45 +0000

    Move ILog to a new file

--- DBS/ObservableLog.cs
+++ DBS/ObservableLog.cs
@@ -8,19 +8,6 @@ using DBS.Utilities;
 
 namespace DBS
 {
-    public interface ILog : IDisposable
-    {
-        void Error(string msg, Exception ex);
-        void ErrorFormat(string format, params object[] args);
-        void Error(string msg);
-        void Info(string msg, Exception ex);
-        void InfoFormat(string format, params object[] args);
-        void Info(string msg);
-        void Custom(string t, string msg, Exception ex);
-        void CustomFormat(string t, string format, params object[] args);
-        void Custom(string t, string msg);
-    }
-
     public class ObservableLog : IObservable<string>
     {
         private readonly Subject<string> _infoSubj = new Subject<string>();
