b9f6c804dbe9de99102f735f1a2103c92a9af53e
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-03-29 17:42:30 +0000

    Add retries to the LookUpProtocol.

--- DBS/Protocols/Enhancements/LookupProtocol.cs
+++ DBS/Protocols/Enhancements/LookupProtocol.cs
@@ -1,63 +1,74 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Reactive.Linq;
-using System.Threading.Tasks;
-using DBS.Messages;
-using DBS.Messages.Enhancements;
-using DBS.Utilities;
+﻿    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Reactive.Linq;
+    using System.Threading.Tasks;
+    using DBS.Messages;
+    using DBS.Messages.Enhancements;
+    using DBS.Utilities;
 
-namespace DBS.Protocols.Enhancements
-{
-    class LookUpProtocol : IProtocol
+    namespace DBS.Protocols.Enhancements
     {
-        private const int WaitPeriod = 10000;
-
-        public Task Run() // should this run in a separate thread? We're deleting files...
+        class LookUpProtocol : IProtocol
         {
-            var backupDir = Core.Instance.Config.BackupDirectory;
-            Core.Instance.Log.InfoFormat("Starting LookUpProtocol: {0}", backupDir);
-            return Task.Factory.StartNew(() =>
+            private const int MaxRetries = 5;
+
+            public Task Run() // should this run in a separate thread? We're deleting files...
             {
-                // get array with all the fileId's associated with the backed up chunks
-                var fileIds = Directory.GetFiles(backupDir, "*_*")
-                    .Select(path =>
+                var backupDir = Core.Instance.Config.BackupDirectory;
+                Core.Instance.Log.InfoFormat("Starting LookUpProtocol: {0}", backupDir);
+                return Task.Factory.StartNew(() =>
+                {
+                    // get array with all the fileId's associated with the backed up chunks
+                    var fileIds = Directory.GetFiles(backupDir, "*_*")
+                        .Select(path =>
+                        {
+                            var fileName = Path.GetFileName(path);
+                            return new FileChunk(fileName).FileId;
+                        });
+
+                    // remove duplicates, transform the collection into a ConcurrentHashSet
+                    var backedUpFiles = new ConcurrentHashSet<FileId>(fileIds.Distinct());
+
+                    var waitPeriod = 1000;
+                    for (int retry = 0; retry < MaxRetries; ++retry)
                     {
-                        var fileName = Path.GetFileName(path);
-                        return new FileChunk(fileName).FileId;
-                    });
+                        // perform a lookup for each fileId
+                        var subscriptions = backedUpFiles.Select(id =>
+                        {
+                            Core.Instance.MCChannel.Send(new LookupMessage(id));
+                            return Core.Instance.MCChannel.Received
+                                .Where(message => message.MessageType == MessageType.Got)
+                                .Cast<GotMessage>()
+                                .Where(message => message.FileId == id)
+                                // ReSharper disable once AccessToDisposedClosure
+                                .Subscribe(msg => backedUpFiles.Remove(msg.FileId));
+                        });
 
-                // remove duplicates, transform the collection into a ConcurrentHashSet
-                var backedUpFiles = new ConcurrentHashSet<FileId>(fileIds.Distinct());
+                        // wait
+                        Task.Delay(waitPeriod).Wait();
 
-                // perform a lookup for each fileId
-                var subscriptions = backedUpFiles.Select(id =>
-                {
-                    Core.Instance.MCChannel.Send(new LookupMessage(id));
-                    return Core.Instance.MCChannel.Received
-                        .Where(message => message.MessageType == MessageType.Got)
-                        .Cast<GotMessage>()
-                        .Where(message => message.FileId == id)
-// ReSharper disable once AccessToDisposedClosure
-                        .Subscribe(msg => backedUpFiles.Remove(msg.FileId));
+                        foreach(var subscription in subscriptions)
+                            subscription.Dispose();
+
+                        if (backedUpFiles.Count == 0)
+                            break;
+
+                        waitPeriod *= 2;
+                    }
+
+                    // delete all the unused chunks
+                    foreach (var unusedFile in backedUpFiles)
+                    {
+                        var fileList = Directory.GetFiles(backupDir, unusedFile + "_*");
+                        foreach (var backedUpChunk in fileList)
+                            File.Delete(backedUpChunk);
+
+                        backedUpFiles.Remove(unusedFile);
+                    }
+
+                    backedUpFiles.Dispose();
                 });
-
-                // wait
-                Task.Delay(WaitPeriod).Wait();
-
-                foreach (var subscription in subscriptions)
-                    subscription.Dispose();
-
-                // delete all the unused chunks
-                foreach (var unusedFile in backedUpFiles)
-                {
-                    var fileList = Directory.GetFiles(backupDir, unusedFile + "_*");
-                    foreach (var backedUpChunk in fileList)
-                        File.Delete(backedUpChunk);
-                }
-
-                backedUpFiles.Dispose();
-            });
+            }
         }
     }
-}
