f7393b09cf57310905c9202b1167e17ec2bf7fa9
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-03-30 11:28:28 +0100

    Add retries to RestoreChunkSubprotocol.

--- DBS/Protocols/RestoreChunkSubprotocol.cs
+++ DBS/Protocols/RestoreChunkSubprotocol.cs
@@ -12,7 +12,7 @@ namespace DBS.Protocols
     class RestoreChunkSubprotocol : IProtocol
     {
         private readonly FileChunk _fileChunk;
-        private const int Timeout = 10000;
+        private const int MaxRetries = 5;
 
         public ChunkMessage Message { get; private set; }
 
@@ -23,23 +23,41 @@ namespace DBS.Protocols
 
         private void SendGetChunk()
         {
-            Core.Instance.MCChannel.Send(new GetChunkMessage(_fileChunk));
+            int timeout = 1000;
+            bool timedOut;
+            int retry = 0;
+            do
+            {
+                timedOut = false;
+                Core.Instance.MCChannel.Send(new GetChunkMessage(_fileChunk));
 
-            try
-            {
-                // wait for response
-                Message = Core.Instance.MDRChannel.Received
-                    .Where(message => message.MessageType == MessageType.Chunk)
-                    .Cast<ChunkMessage>()
-                    .Where(message => message.ChunkNo == _fileChunk.ChunkNo &&
-                        message.FileId == _fileChunk.FileId)
-                    .Timeout(TimeSpan.FromMilliseconds(Timeout))
-                    .Next().First();
-            }
-            catch (TimeoutException)
-            {
-                Core.Instance.Log.ErrorFormat("RestoreChunkSubprotocol: Could not fetch {0} from the network.", _fileChunk);
-            }
+                try
+                {
+                    // wait for response
+                    Message = Core.Instance.MDRChannel.Received
+                        .Where(message => message.MessageType == MessageType.Chunk)
+                        .Cast<ChunkMessage>()
+                        .Where(message => message.ChunkNo == _fileChunk.ChunkNo &&
+                                          message.FileId == _fileChunk.FileId)
+                        .Timeout(TimeSpan.FromMilliseconds(timeout))
+                        .Next().First();
+                }
+                catch (TimeoutException)
+                {
+                    timedOut = true;
+                    ++retry;
+                    timeout *= 2;
+                    Core.Instance.Log.ErrorFormat(
+                        "RestoreChunkSubprotocol: Could not fetch {0} from the network. Retrying.",
+                        _fileChunk);
+                }
+
+            } while (timedOut && retry < MaxRetries);
+
+            if (timedOut)
+                Core.Instance.Log.ErrorFormat(
+                    "RestoreChunkSubprotocol: Could not fetch {0} from the network. Giving up.",
+                    _fileChunk);
         }
 
         public Task Run()
